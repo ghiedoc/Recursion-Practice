3b7e34f0a1a97cea4c16eda08f3ed553593d59cf
Gillian Joanna Doctor <ghiedoc@gmail.com>
2018-10-01 17:06:19 +0800

    Recursion new practice

--- src/practicce/rec/PracticeRecur.java
+++ src/practicce/rec/PracticeRecur.java
@@ -27,11 +27,12 @@ public class PracticeRecur {
         System.out.println("Method " + n);
 
         if (n <= 1) {
+            System.out.println("Returned 1");
             return 1;
         } else {
             int result = factorial(n - 1) * n;
             System.out.print("Returned " + result);
-            System.out.println(": " + n + " factorial(" + n + "-1)");
+            System.out.println(": " + n + " * factorial(" + n + "-1)");
 
             return result;
 
@@ -44,16 +45,17 @@ public class PracticeRecur {
      * @return the fibonacci of a given number
      */
     public static int fibonacci(int n) {
-        
+
         System.out.println("Method " + n);
-        
+
         if (n < 2) {
+            System.out.println("Returned 1");
             return 1;
         } else {
             int result = fibonacci(n - 1) + fibonacci(n - 2);
             System.out.print("Returned " + result);
-            System.out.println(": " + n + " fibonacci(" + n + "-1)");
-            
+            System.out.println(": " + "fibonacci(" + n + "-1) + " + "fibonacci(" + n + "-2)");
+
             return result;
         }
     }
@@ -64,6 +66,9 @@ public class PracticeRecur {
      * @return the bunnies of a given number
      */
     public static int bunnyEars2(int bunnies) {
+
+        System.out.println("Method " + bunnies);
+
         if (bunnies == 0) {
             return 0;
         } else if (bunnies % 2 == 0) {
@@ -78,12 +83,19 @@ public class PracticeRecur {
     }
 
     public static int triangle(int rows) {
-        if (rows == 0) {
-            return 0;
-        } else if (rows == 1) {
+
+        System.out.println("Method " + rows);
+
+        if (rows == 1) {
+            System.out.println("Returned 1");
             return 1;
+            
         } else {
-            return triangle(rows - 1) + rows;
+            int result = triangle(rows - 1) + rows;
+            System.out.print("Returned " + result);
+            System.out.println(": " + rows + " + triangle(" + rows + "-1)");
+
+            return result;
         }
     }
 
@@ -121,9 +133,25 @@ public class PracticeRecur {
         }
         System.out.println(sum);
     }
+    
+    public static int power(int base, int exponent){
+        int result = 0;
+        
+        if(exponent == 0){
+
+            return  1;
+        }
+        else if(exponent != 0){
+            result =  (base * power(base, exponent-1));
+            //System.out.println(result);
+            
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(5));
+        System.out.println(power(2,4));
+        
     }
 
 }
